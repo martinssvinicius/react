7aa1b68c00ca67d252490271281609b7d9046119
martinssvinicius <martinssvinicius@hotmail.com>
2023-10-15 12:24:19 -0300

    teste

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -19,7 +19,7 @@ export default function FirstPost() {
             />
             <h1>First Post</h1>
             <h2>
-                <Link href="/">Back to home</Link>
+                <Link href="/">Back to homee</Link>
             </h2>
         </Layout>
     );
